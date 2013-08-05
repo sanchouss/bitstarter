b0f5b601835d2811c1634a48514b6a2dcf96e1a1
Alexander Perepelkin <ubuntu@ip-172-31-45-135.us-west-2.compute.internal>
2013-08-05 12:55:29 +0000

    buff

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    var buffer = fs.readFileSync('index.html');
+    response.send(buffer.toString();
 });
 
 var port = process.env.PORT || 5000;
